04b8e38bcadf5d67195eedc6b3a46c7707100806
Krishna Kumar Mandal <mandalkrishna739@gmail.com>
2024-06-12 15:38:53 +0545

    js modules

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let sumData = (a, b) => {
+    return a + b;
+}
+let subData = (a, b) => {
+    return a - b;
+}
+let prodData = (a, b) => {
+    return a * b;
+}
+let divData = (a, b) => {
+    return a / b;
+}
+
+// export default sumData; //can only export one function
+export { sumData, subData, prodData, divData };
---
+++ index.js
@@ -0,0 +1,15 @@
+import { sumData, subData, prodData, divData } from "./calculator.js";
+
+let sum = sumData(4, 7)
+console.log(sum);
+
+let sub = subData(9, 2)
+console.log(sub);
+
+let prod = prodData(4, 7)
+console.log(prod);
+
+
+let div = divData(20, 4)
+console.log(div);
+
